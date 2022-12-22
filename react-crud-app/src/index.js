2052b8a7c938fbacff60f2f7a9f123bb055e81a7
Jon Haataja <jon.haataja@student.careeria.fi>
2022-12-22 21:24:16 +0200

    Added React-dom, React-router-dom and react-bootstrap ThemeProvider to index.js

--- react-crud-app/src/index.js
+++ react-crud-app/src/index.js
@@ -1,14 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./assets/App.scss";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import './assets/App.scss';
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter as Router} from 'react-router-dom';
+import App from './App';
+import ThemeProvider from 'react-bootstrap/ThemeProvider';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <React.StrictMode>
+      <ThemeProvider
+        breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
+        minBreakpoint="xxs">
+          <App />
+      </ThemeProvider>
+    </React.StrictMode>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
