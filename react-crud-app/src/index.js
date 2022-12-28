9c7704ae47add040bff9b1a289c48900beab365b
Jon Haataja <jon.haataja@student.careeria.fi>
2022-12-28 13:58:13 +0200

    Removed React Router from index.js, added empty pages.

--- react-crud-app/src/index.js
+++ react-crud-app/src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './assets/App.scss';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter as Router} from 'react-router-dom';
 import App from './App';
 import ThemeProvider from 'react-bootstrap/ThemeProvider';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router>
     <React.StrictMode>
       <ThemeProvider
         breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
@@ -16,7 +14,6 @@ root.render(
           <App />
       </ThemeProvider>
     </React.StrictMode>
-  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
