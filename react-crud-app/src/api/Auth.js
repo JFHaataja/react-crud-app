f62bc5d6cb826c0965ab9cc04386173d601fc7b3
Jon Haataja <jon.haataja@student.careeria.fi>
2023-01-01 21:44:25 +0200

    Moved API files to another folder. Added Axios API functions.

---
+++ react-crud-app/src/api/Auth.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+// second localhost baseUrl is for testing purposes (remember to comment it out when done testing)
+
+// const baseUrl = "https://backendnw.azurewebsites.net/nw/authentication"
+const baseUrl = 'https://localhost:7109/nw/authentication';
+
+const authenticate = (userForAuth) => {
+ const request = axios.post(baseUrl, userForAuth);
+ return request.then((response) => response);
+};
+
+export default { authenticate };
