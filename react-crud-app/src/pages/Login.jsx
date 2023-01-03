08f57eef8200ddfa8c44ae5782ca2a68a0cfa58b
Jon Haataja <jon.haataja@student.careeria.fi>
2023-01-03 16:23:29 +0200

    Added pages and their code.

--- react-crud-app/src/pages/Login.jsx
+++ react-crud-app/src/pages/Login.jsx
@@ -1,101 +1,116 @@
-import 'assets/App.scss'
-import { useState } from 'react'
-import md5 from 'md5'
-import LoginService from 'services/Auth'
-import Button from 'components/elements/Button/PrimaryButton/PrimaryButton'
-import Form from 'react-bootstrap/Form'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Spinner from 'components/elements/Spinner/Spinner'
-import LogIn from 'components/elements/Icon/LogIn'
-import Cart from 'components/elements/Icon/Cart'
+import 'assets/App.scss';
+import { useState } from 'react';
+import md5 from 'md5';
+import LoginService from 'api/Auth';
+import Button from 'components/elements/Button/PrimaryButton/PrimaryButton';
+import Form from 'react-bootstrap/Form';
+import FormGroup from 'components/elements/Form/FormGroup/FormGroup/FormGroup';
+import FormGroupPassword from 'components/elements/Form/FormGroup/FormGroup/FormGroupPassword';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Spinner from 'components/elements/Spinner/Spinner';
+import LogIn from 'assets/Icon/LogIn';
+import Cart from 'assets/Icon/Cart';
 
-const Login = ({setIsPositive, setMessage, setShowMessage, setLoggedInUser, setAdminUser}) => {
+const Login = ({ setIsPositive, setMessage, setShowMessage, setLoggedInUser, setAdminUser }) => {
+ const [username, setUsername] = useState('');
+ const [password, setPassword] = useState('');
+ const [buttonText, setButtonText] = useState('Login');
 
-const [username, setUsername] = useState('')
-const [password, setPassword] = useState('')
-const [buttonText, setButtonText] = useState('Login')
+ const handleSubmit = (event) => {
+  event.preventDefault();
+  const userForAuth = {
+   username: username,
+   password: md5(password),
+  };
+  setButtonText(<Spinner spinnerSize={'sm'} spinnerVariant="dark" />);
 
-const handleSubmit = (event) => {
-      event.preventDefault()
-      const userForAuth = {
-        username: username,
-        password: md5(password)  
+  LoginService.authenticate(userForAuth)
+   .then((response) => {
+    if (response.status === 200) {
+     localStorage.setItem('username', response.data.username);
+     localStorage.setItem('accesslevelId', response.data.accesslevelId);
+     localStorage.setItem('token', response.data.token);
+
+     setLoggedInUser(response.data.username);
+
+     if (response.data.accesslevelId === 1) {
+      setAdminUser(response.data.accesslevelId);
+     } else {
+      setAdminUser(null);
+     }
+     setMessage(`Logged in as: ${userForAuth.username}`);
+     setIsPositive(true);
+     setShowMessage(true);
+
+     setTimeout(() => {
+      setShowMessage(false);
+     }, 5000);
     }
-    setButtonText(<Spinner spinnerSize={'sm'} spinnerVariant="dark"/>)
-    
-    LoginService.authenticate(userForAuth)
-    .then(response => {
-      if (response.status === 200) {
+   })
+   .catch((error) => {
+    setMessage(error);
+    setIsPositive(false);
+    setShowMessage(true);
 
-        localStorage.setItem("username", response.data.username)
-        localStorage.setItem("accesslevelId", response.data.accesslevelId)
-        localStorage.setItem("token", response.data.token)
-        
-        setLoggedInUser(response.data.username)
+    setTimeout(() => {
+     setShowMessage(false);
+    }, 6000);
+   });
+ };
 
-        if(response.data.accesslevelId === 1){
-          setAdminUser(response.data.accesslevelId)
-        }else {
-          setAdminUser(null)
-        }
-       setMessage(`Logged in as: ${userForAuth.username}`)
-       setIsPositive(true)
-       setShowMessage(true)
-       
-      
-       setTimeout(() => {
-        setShowMessage(false)
-       }, 5000)
-    }
+ const emptyFields = () => {
+  setUsername('');
+  setPassword('');
+ };
 
-      })
-      .catch(error => {
-        setMessage(error)
-        setIsPositive(false)
-        setShowMessage(true)
+ return (
+  <Container fluid>
+   <Row>
+    <Col md={7} className="overflow-hidden"></Col>
+    <Col className="vh-100 d-flex justify-content-center bg-white shadow-lg overflow-hidden">
+     <Form onSubmit={handleSubmit} className="text-start d-flex flex-column justify-content-center">
+      <p className="fs-6 fw-bold text-primary mb-0 pb-0">
+       NW TRADERS <Cart />
+      </p>
+      <h1 className="fs-2">Welcome!</h1>
+      <p>Please Log in to check out my demo app.</p>
 
-        setTimeout(() => {
-          setShowMessage(false)
-         }, 6000)
-      })
-    }
+      <FormGroup
+       formLabelText='Username'
+       inputType="text"
+       inputValue={username}
+       inputPlaceholder="First Name"
+       onChangeHandler={({ target }) => setUsername(target.value)}
+       requiredOrNot={true}
+      />
 
-const emptyFields = () => {
-    setUsername("")
-    setPassword("")
-}
+      <FormGroupPassword
+       passwordType='password'
+       passwordValue={password}
+       passwordPlaceholder="Password"
+       onChangeHandler={({ target }) => setPassword(target.value)}
+       requiredOrNot={true}
+       dataCy="passwordLogin"
+       formControlId={"passwordLogin"}
+      />
 
-  return (
-    <Container fluid>
-      <Row>
-        <Col md={7} className='overflow-hidden'></Col>
-        <Col className='vh-100 d-flex justify-content-center bg-white shadow-lg overflow-hidden'>
-          <Form onSubmit={handleSubmit} className="text-start d-flex flex-column justify-content-center">
-            <p className='fs-6 fw-bold text-primary mb-0 pb-0'>NW TRADERS <Cart/></p>          
-            <h1 className='fs-2'>Welcome!</h1>
-            <p>Please Log in to check out my demo app.</p>
+      <Form.Group>
+       <Button btnVariant="outline-primary" clickHandler={() => emptyFields()} buttonText="Clear" />
+       <Button
+        btnVariant="primary"
+        btnType="submit"
+        buttonText={buttonText}
+        clickHandler={() => setButtonText()}
+        endIcon={<LogIn />}
+       />
+      </Form.Group>
+     </Form>
+    </Col>
+   </Row>
+  </Container>
+ );
+};
 
-            <Form.Group className="mb-3 mt-2" controlId="username">
-              <Form.Label className='pb-0'>Username</Form.Label>
-              <Form.Control value={username} placeholder="Username" onChange={({ target }) => setUsername(target.value)} required/>
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="password">
-              <Form.Label className='pb-0'>Password</Form.Label>
-              <Form.Control value={password} type="password" placeholder="Password" onChange={({ target }) => setPassword(target.value)} required/>
-            </Form.Group>
-
-            <Form.Group>
-              <Button btnVariant="outline-primary" clickHandler={() => emptyFields()} buttonText="Clear"/>
-              <Button btnVariant="primary" btnType="submit" buttonText={buttonText} clickHandler={() => setButtonText()} endIcon={<LogIn/>}/>         
-            </Form.Group>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
-  )
-}
-
-export default Login
+export default Login;
--- react-crud-app/src/pages/Posts.jsx
+++ react-crud-app/src/pages/Posts.jsx
@@ -1,47 +1,59 @@
-import {useState, useEffect} from 'react'
-import 'assets/App.scss'
-import { Container } from 'react-bootstrap'
-import Post from 'components/elements/Card/Post'
+import { useState, useEffect } from 'react';
+import 'assets/App.scss';
+import { Container } from 'react-bootstrap';
+import Post from 'components/elements/Card/Post';
 
 const Posts = () => {
+ const [posts, setPosts] = useState([]);
 
-    const [posts, setPosts] = useState([])
+ useEffect(() => {
+  fetch('https://jsonplaceholder.typicode.com/posts')
+   .then((res) => res.json())
+   .then((oliot) => setPosts(oliot));
+ }, []);
 
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(res => res.json())
-        .then(oliot => setPosts(oliot))
-    },[])
-
-  return (
-    <>
-    <Container fluid className='bg-CustomersHeroCard h-25 py-2'>
-        <div className='pt-5'>
-            <h1 className='display-3 text-white'>Posts <span>&#128195;</span></h1>
-            <p className='fs-5 fw-semibold'>Here are some cards made by fetching example data from <a className='link-primary' target={"_blank"} rel={'noreferrer'} href="https://jsonplaceholder.typicode.com/">JSONPlaceholder</a></p>
-        </div>
-    </Container>
-
-       <div className='container my-5'>  
-       <div className='row'>
-        {       
-            posts && posts.map(post =>
-              <div className='col-lg-4 mb-5' key={post.id}>
-                <div className='h-100'>
-                      <Post
-                      postImg={'https://images.unsplash.com/photo-1637614052127-80276701a4e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80'}
-                      postKey={post.id}
-                      cardTitle={post.title}
-                      cardText={post.body}
-                      />
-                </div>
-              </div>             
-                )          
-        }  
-        </div>  
+ return (
+  <>
+   <Container fluid className="bg-drinks h-25 py-2">
+    <div className="pt-5">
+     <h1 className="display-3 text-white">
+      Posts <span>&#128195;</span>
+     </h1>
+     <p className="fs-5 fw-semibold">
+      Here are some cards made by fetching example data from{' '}
+      <a
+       className="link-primary"
+       target={'_blank'}
+       rel={'noreferrer'}
+       href="https://jsonplaceholder.typicode.com/"
+      >
+       JSONPlaceholder
+      </a>
+     </p>
     </div>
-    </>
-  )
-}
+   </Container>
 
-export default Posts
+   <div className="container my-5">
+    <div className="row">
+     {posts &&
+      posts.map((post) => (
+       <div className="col-lg-4 mb-5" key={post.id}>
+        <div className="h-100">
+         <Post
+          postImg={
+           'https://images.unsplash.com/photo-1637614052127-80276701a4e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80'
+          }
+          postKey={post.id}
+          cardTitle={post.title}
+          cardText={post.body}
+         />
+        </div>
+       </div>
+      ))}
+    </div>
+   </div>
+  </>
+ );
+};
+
+export default Posts;
