6ae1525125b5ac06caa04018558084f141647a4a
Jon Haataja <jon.haataja@student.careeria.fi>
2023-01-03 16:29:40 +0200

    Added and ran cypress E2E tests.

---
+++ react-crud-app/cypress.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+ e2e: {
+  setupNodeEvents(on, config) {
+   // implement node event listeners here
+  },
+  experimentalFetchPolyfill: true,
+ },
+});
